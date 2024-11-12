72570b87123936579d77bed50dac5c8789b08499
kevin kalenga <kevinkalenga10@gmail.com>
2024-11-12 11:57:29 +0100

    Create environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
-import products from './data/products.js' 
-const port = 5000;
+import products from './data/products.js';
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 
