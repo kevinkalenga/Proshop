a682278d951b85e848da320c6cf9a020c1b5256a
kevin kalenga <kevinkalenga10@gmail.com>
2024-11-14 23:41:45 +0100

    Item Count In Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import {Navbar, Nav, Container, NavbarToggle, NavbarCollapse} from 'react-bootstrap';
+import {Navbar, Nav, Container, NavbarToggle, Badge} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
 import {LinkContainer} from 'react-router-bootstrap'
+import { useSelector, useDispatch } from 'react-redux';
 import logo from '../assets/logo.png'
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
@@ -18,7 +20,14 @@ const Header = () => {
                 <Navbar.Collapse id="basic-navbar-nav" >
                     <Nav className='ms-auto'>
                       <LinkContainer to='/cart' >
-                        <Nav.Link href='/cart'><FaShoppingCart />Cart</Nav.Link>
+                        <Nav.Link href='/cart'>
+                          <FaShoppingCart />Cart
+                          {cartItems.length > 0 && (
+                             <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                                   {cartItems.reduce((a, c) => a + c.qty, 0)}
+                             </Badge>
+                )}
+                          </Nav.Link>
                        </LinkContainer>
                        <LinkContainer to='/login' >
                          <Nav.Link ><FaUser />Sing In</Nav.Link>
