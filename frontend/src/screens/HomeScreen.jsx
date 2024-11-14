c2ba9c5ba7df1aa8b141ed892bbb2e9187d2a5c9
kevin kalenga <kevinkalenga10@gmail.com>
2024-11-14 03:34:45 +0100

    Loader and Message Components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,9 @@
 
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
-import {useGetProductsQuery} from '../slices/productsApiSlice'
+import {useGetProductsQuery} from '../slices/productsApiSlice';
+import Loader from "../components/Loader";
+import Message from "../components/Message"
 
 
 
@@ -14,8 +16,8 @@ const HomeScreen = () => {
     <>
       {
         isLoading ? (
-          <h2>Loading...</h2>
-        ) : error ? (<div>{error?.data?.message || error.error}</div>) : 
+          <Loader />
+        ) : error ? (<Message variant="danger">{error?.data?.message || error.error}</Message>) : 
         (<>
         
         <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,7 +2,9 @@ import {useParams} from "react-router-dom";
 import { Link } from "react-router-dom";
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from '../components/Rating';
-import {useGetProductDetailsQuery} from '../slices/productsApiSlice'
+import {useGetProductDetailsQuery} from '../slices/productsApiSlice';
+import Loader from "../components/Loader";
+import Message from "../components/Message"
 
 
 const ProductScreen = () => {
@@ -20,9 +22,9 @@ const ProductScreen = () => {
        
         {
           isLoading ? (
-             <h2>Loading...</h2>
+             <Loader />
           ) : error ? (
-            <div>{error?.data?.message || error.error}</div>
+            <Message variant="danger">{error?.data?.message || error.error}</Message>
           ) : (
 
             <Row>
