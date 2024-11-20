1278fa07db52030cb79570fb1ac24effd68a9bac
kevin kalenga <kevinkalenga10@gmail.com>
2024-11-20 04:13:02 +0100

    Auth middleware and Endpoint

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -10,15 +10,17 @@ import { authUser,
     getUserById,
     updateUser } from "../controllers/userController.js";
 
+    import {protect, admin} from '../middleware/authMiddleware.js';
+
 
 
 // router.get('/', getProducts);
 // router.get('/:id', getProductById);
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
-router.post('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser)
+router.post('/auth', authUser);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 
 export default router;
