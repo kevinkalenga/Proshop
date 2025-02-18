1b7d3026b4112ae7e4487e72af5a6383d0f248b2
kevin kalenga <kevinkalenga10@gmail.com>
2025-02-18 01:23:38 +0100

    React-PayPal Integration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import {
-      createBrowserRouter, 
-      createRoutesFromElements, 
-      Route, 
-      RouterProvider
-    } from "react-router-dom";
-import {Provider} from "react-redux";
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+import { Provider } from "react-redux";
 import store from "./store";
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -20,7 +21,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
-import PrivateRoute from './components/PrivateRoute' ;
+import PrivateRoute from './components/PrivateRoute';
 import PaymentScreen from './screens/PayementScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
@@ -29,25 +30,25 @@ import OrderScreen from './screens/OrderScreen';
 const router = createBrowserRouter(
   createRoutesFromElements(
 
-  
-    <Route path="/" element={<App />}>
-       <Route index={true} path="/" element={<HomeScreen />} />
-       <Route  path="/product/:id" element={<ProductScreen />} />
-       <Route  path="/cart" element={<CartScreen />} />
-       <Route path='/login' element={<LoginScreen />} />
-       <Route path='/register' element={<RegisterScreen />} />
-       <Route path='' element={<PrivateRoute />} >
-         <Route path='/shipping' element={<ShippingScreen />} />
-         <Route path='/payment' element={<PaymentScreen />} />
-         <Route path='/placeorder' element={<PlaceOrderScreen />} />
-         <Route path='/order/:id' element={<OrderScreen />} />
-       </Route>
-       
-    </Route>
-    
-    
 
-  
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      <Route path='' element={<PrivateRoute />} >
+        <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
+        <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
+      </Route>
+
+    </Route>
+
+
+
+
   )
 )
 
@@ -55,7 +56,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
