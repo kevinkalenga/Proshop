3175ced8d27c4d3f8a65b8e128ea8ce6a95ef06f
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-23 16:22:16 +0100

    Create Reviews -Backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from "../controllers/productController.js";
+import {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct, createProductReview
+} from "../controllers/productController.js";
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 
@@ -9,6 +15,7 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 // router.get('/:id', getProductById);
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview)
 
 
 export default router;
