c26860187811d3021abecfbba1d3e295aa739dc8
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-24 08:38:34 +0100

    Paginate Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,41 +1,41 @@
-
-import {Row, Col} from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import {useGetProductsQuery} from '../slices/productsApiSlice';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from "../components/Loader";
 import Message from "../components/Message"
 
 
 
 const HomeScreen = () => {
-  
-  const {data: products, isLoading, error} = useGetProductsQuery()
-  
-  
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber })
+
+
   return (
     <>
       {
         isLoading ? (
           <Loader />
-        ) : error ? (<Message variant="danger">{error?.data?.message || error.error}</Message>) : 
-        (<>
-        
-        <h1>Latest Products</h1>
-       <Row>
-         {
-            products.map((product) =>(
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+        ) : error ? (<Message variant="danger">{error?.data?.message || error.error}</Message>) :
+          (<>
+
+            <h1>Latest Products</h1>
+            <Row>
+              {
+                data.products.map((product) => (
+                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product} />
-                </Col>
-            ))
-         }
-       </Row>
-        
-        
-        </>)
+                  </Col>
+                ))
+              }
+            </Row>
+
+
+          </>)
       }
-      
-      
+
+
     </>
   )
 }
